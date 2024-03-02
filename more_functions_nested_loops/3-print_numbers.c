c32b87894cc6d874e70346b17f102141694c06f7
mal <159127791+maliluu@users.noreply.github.com>
2024-03-02 13:35:35 +0200

    Update 3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,13 @@
 /**
- * print_most_numbers - print all number from 0 to 9 expext 2 and 4.
+ * print_numbers - a function that prints the numbers, from 0 to 9
  *
- * Return:void
+ * Return: 0-9 followed by newline
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
-int i;
+	int i;
 
-	for (i = 48; i <= 57; i++)
-	{
-		if ((i != 50) && (i != 52))
-			_putchar(i);
-	}
-_putchar('\n');
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
 }
