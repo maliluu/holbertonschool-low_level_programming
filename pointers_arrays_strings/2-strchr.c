853c78b01b025f4f09b4d5cf73bd5a4d397fe916
mal <159127791+maliluu@users.noreply.github.com>
2024-03-05 17:44:54 +0200

    Update 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success
  */
 
 char *_strchr(char *s, char c)
