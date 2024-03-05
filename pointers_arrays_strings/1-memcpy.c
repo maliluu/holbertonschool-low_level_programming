e9fe113e4282adfaef296145e2d4d47b1c039959
mal <159127791+maliluu@users.noreply.github.com>
2024-03-05 17:43:46 +0200

    !

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * _memcpy - Entry point
- * @dest:put
- * @src: input
- * @n: input
- * Return: Always 0 (Success)
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
