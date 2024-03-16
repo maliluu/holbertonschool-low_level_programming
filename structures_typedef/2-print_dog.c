8ee8e7b0f96d1efd76624301d55d3215639e0bb0
mal <159127791+maliluu@users.noreply.github.com>
2024-03-16 16:30:27 +0200

    Update 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,21 +1,28 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * print_dog - prints dog stats
- * @d: dog to print
+ * print_dog - Function that prints a dog. That is, its name, age, and owner
+ * @d: Contains a struct holding various variables
+ * Return: Nothing
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->age <= 0)
-		printf("Age: (nil)\n");
-	if (d->owner == NULL)
-		d->owner = "(nil)";
+	if (d != NULL)
+	{
+		printf("Name: ");
+		if (d->name == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", d->name);
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+		printf("Age: %f\n", d->age);
+
+		printf("Owner: ");
+		if (d->owner == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", d->owner);
+	}
 }
